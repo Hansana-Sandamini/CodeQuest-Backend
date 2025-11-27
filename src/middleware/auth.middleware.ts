096620e2fab1a5b69480a382bbc1bad83e3ecb2d
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-11-27 19:46:31 +0530

    Implement complete JWT authentication with proper typing, Bearer check

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,5 +1,11 @@
-import { NextFunction, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { Role } from "../models/user.model"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const JWT_SECRET = process.env.JWT_SECRET as string
 
 export interface AuthRequest extends Request {
     user?: any
@@ -23,4 +29,25 @@ export const requireRole = (allowedRoles: Role[]) => {
 
     next()
   }
+}
+
+export const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).json({message: "No token provided"})
+    }
+
+    // Bearer rtyuilkjkhpoiuytsdfghjk
+    const token = authHeader.split(" ")[1]   
+
+    try {
+        const payload = jwt.verify(token, JWT_SECRET)
+        req.user = payload
+        next()
+
+    } catch (err) {
+        console.error(err)
+        return res.status(403).json({message: "Invalid or expired token"})
+    }
 }
